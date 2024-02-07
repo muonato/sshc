d9cabd4bea660fe491a0fb25051724995885a0fc
Tommi Muona <43796235+muonato@users.noreply.github.com>
2024-02-07 08:28:42 +0200

    Simple helper to send command to host(s) over ssh

---
+++ sshc.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# muonato/sshc.sh @ GitHub 07-FEB-2024
+#
+# Simple helper to send command to host(s) over ssh,
+# assumes login with ssh-agent (or without password)
+#
+# Usage:
+#       bash sshc.sh <hostsfile>|<hostname> <command>
+#
+# Parameters:
+#       1: Hostname or path to filename
+#       2: Command to send over ssh
+#
+# Examples:
+#       $ bash sshc.sh hosts.txt "uptime"
+#       = query uptime of hosts listed in file
+#
+#       $ bash lssw.sh server.domain.net "cat /etc/os-release"
+#       = view OS release of specified hostname
+#
+#       The 'hostsfile' should have one hostname per row
+#
+#       NOTICE: Verify the command to execute carefully !
+#
+: ${1?"ERROR: missing hostname or filename"}
+: ${2?"ERROR: missing command to send over ssh"}
+
+function send_command() {
+    echo -e "\n$HOST\n=============="
+    ssh $HOST $CMD
+}
+
+CMD=$2
+
+# Test for hostname file
+if [[ -f "$1" ]]; then
+   while read HOST ; do
+        echo "" | send_command ;
+    done < $1
+else
+    HOST=$1
+    echo "" | send_command ;
+fi
