b11063c89fb9f8113e990870aa8eab8da223df69
Tommi Muona <43796235+muonato@users.noreply.github.com>
2024-02-12 12:04:01 +0200

    Simple helper to ssh commandline to hosts

--- sshc.sh
+++ sshc.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-# muonato/sshc @ GitHub 11-FEB-2024
+# muonato/sshc @ GitHub 12-FEB-2024
 #
-# Simple helper to send ssh command to hosts, assumes 
-# login with ssh-agent (or without password).
+# Simple helper to send commandline to host(s) over ssh,
+# assumes login with ssh-agent (or without password).
 # Hosts may be grouped with labels in brackets and listed
-# one-per-row in a hostsfile.
+# one-per-row in a hostsfile. Comment rows # are ignored.
 #
 # Usage:
-#       bash sshc.sh <host | hostsfile> <command> [group]
+#       bash sshc.sh <host | hostsfile> <command> <group>
 #
 # Parameters:
 #       1:  Hostname or path to file (required)
@@ -33,8 +33,11 @@
 #       3. List hosts in the hostsfile group 'webhost'
 #           $ ./sshc.sh hosts.txt "check" "webhost"
 #
-#       4. Show version of Apache in Docker container of hostsfile group 'webhost'
-#           $ ./sshc.sh hosts.txt "sudo docker exec my-container httpd -v" "webhost"
+#       4. Show version of Apache in hostfile group 'webhost'
+#           $ ./sshc.sh hosts.txt "httpd -v" webhost
+#
+#       5. Show version of Apache in Docker container
+#           $ ./sshc.sh hosts.txt "sudo docker exec my-container httpd -v"
 #
 : ${1?"ERROR: missing hostname or filename"}
 : ${2?"ERROR: missing command string to execute"}
@@ -45,26 +48,32 @@ COMND=$2
 LABEL=$3
 
 if [[ -f "$HOSTS" ]]; then
-    while read ENTRY; do
-        BLOCK=$(echo $ENTRY|grep -e '^\[.*\]')
-        if [[ $BLOCK ]]; then
-            MATCH=$(echo "$ENTRY"|grep "$LABEL")
-            if [[ $MATCH ]]; then
-                GROUP="TRUE"
-                ENTRY=""
-            else
-                GROUP=""
-            fi
-        fi
-        if [[ $GROUP && $ENTRY ]]; then
-            if [[ $COMND == "check" ]]; then
-                echo -e "\033[1;36m$ENTRY\033[0m"
-            else
-                echo -e "\n[\033[1;36m$ENTRY\033[0m]"
-                echo "" | ssh $ENTRY $COMND;
-            fi
-        fi
-    done < $HOSTS
+        while read ENTRY; do
+                # skip comment rows
+                ENTRY=$(echo $ENTRY|grep -v '^#')
+
+                # identify group tags
+                BLOCK=$(echo $ENTRY|grep -e '^\[.*\]')
+
+                if [[ $BLOCK ]]; then
+                        # match requested group
+                        MATCH=$(echo "$ENTRY"|grep "$LABEL")
+                        if [[ $MATCH ]]; then
+                                GROUP="TRUE"
+                                ENTRY=""
+                        else
+                                GROUP=""
+                        fi
+                fi
+                if [[ $GROUP && $ENTRY ]]; then
+                        if [[ $COMND == "check" ]]; then
+                                echo -e "\033[1;36m$ENTRY\033[0m"
+                        else
+                                echo -e "\n[\033[1;36m$ENTRY\033[0m]"
+                                echo "" | ssh $ENTRY $COMND;
+                        fi
+                fi
+        done < $HOSTS
 else
-    echo "" | ssh $HOSTS $COMND;
+        echo "" | ssh $HOSTS $COMND;
 fi
